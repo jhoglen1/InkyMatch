5f7b50273b6f6777c79cfb4dfe37ffb345ed633d
Jeffrey Hoglen <jeffhoglen@gmail.com>
2022-05-23 11:56:27 -0400

    code checked in

--- inky-match/src/app/card/card.component.ts
+++ inky-match/src/app/card/card.component.ts
@@ -18,18 +18,21 @@ export class CardComponent implements OnInit {
   showFirstTryMessage: boolean = false;
 
   constructor() { }
-  
+
   ngOnInit(): void {
     this.startGameModal();
-    this.cardFlip(); 
+    this.cardFlip();
+    this.shuffleCards();
+
   }
-// on page load show modal and onclick of start button close modal
-  startGameModal(): void  {
+  // on page load show modal and onclick of start button close modal
+  startGameModal(): void {
     let modal = document.getElementById("startModal");
     modal.style.display = "block";
     document.getElementById('startButton').addEventListener('click', () => {
       modal.style.display = "none"
     })
+
   }
   /// gets the flipped card value 
   // loops thru the array of cards 
@@ -41,20 +44,20 @@ export class CardComponent implements OnInit {
   // takes triescount and divide by 2 to get how many tries they have had
   // if triescount is 6 so first try message if not show try again message
   // then after timeout the game will restart
-  cardFlip(): void  { 
+  cardFlip(): void {
     let selectedCard = document.querySelectorAll('.base-card');
     selectedCard.forEach(card => card.addEventListener('click', () => {
-     this.count++;
-     this.triesCount++;
-      this.whichColor === undefined ?(this.reventCard = card,this.whichColor = card.getAttribute("data-intent")):(this.whichOtherColor = card.getAttribute("data-intent"),this.otherRevertCard = card);
+      this.count++;
+      this.triesCount++;
+      this.whichColor === undefined ? (this.reventCard = card, this.whichColor = card.getAttribute("data-intent")) : (this.whichOtherColor = card.getAttribute("data-intent"), this.otherRevertCard = card);
       card.classList.add('is-flipped');
       if (this.whichColor !== undefined && this.whichOtherColor !== undefined) {
         this.doTheyMatch()
       }
       if (this.count === 12) {
-       this.triesCount = this.triesCount / 2;
-       this.triesCount === 6 ? this.showFirstTryMessage = true : this.showFirstTryMessage = false;
-       console.log(this.showFirstTryMessage);
+        this.triesCount = this.triesCount / 2;
+        this.triesCount === 6 ? this.showFirstTryMessage = true : this.showFirstTryMessage = false;
+        console.log(this.showFirstTryMessage);
         setTimeout(() => {
           this.reStartGameModal()
         }, 2000);
@@ -65,21 +68,29 @@ export class CardComponent implements OnInit {
   // checks to see if the cards match - if they do the event listner is removed to both cards - if not the count is decreased by 2 and the card is flipped back over
   // after the timeout is done so the user can see the cards
   // resets the variable to undfined to check it again when clicked
-  doTheyMatch(): void  {  
+  doTheyMatch(): void {
     this.whichColor === this.whichOtherColor ? (this.reventCard.removeEventListener('click', this.cardFlip), this.otherRevertCard.removeEventListener('click', this.cardFlip)) : setTimeout(() => {
-      this.count-=2;
+      this.count -= 2;
       this.reventCard.classList.remove('is-flipped')
       this.otherRevertCard.classList.remove('is-flipped');
     }, 2000);
     this.whichColor = undefined;
     this.whichOtherColor = undefined;
   }
-// shows the score modal and then restarts after the timeout
-  reStartGameModal(): void  {
+  // shows the score modal and then restarts after the timeout
+  reStartGameModal(): void {
     let modal = document.getElementById("endModal");
     modal.style.display = "block";
     setTimeout(() => {
       location.reload();
     }, 6500);
+  }
+
+// shuffles the cards at the start of the game every time.
+  shuffleCards() {
+    let list = document.getElementById("cardWraps");
+    for (var i = list.children.length; i >= 0; i--) {
+      list.appendChild(list.children[Math.random() * i | 0]);
+    }
   }
 }
