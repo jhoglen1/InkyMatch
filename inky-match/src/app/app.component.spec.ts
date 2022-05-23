184e5fb38d953e78635f432a5baa5db584651547
Jeffrey Hoglen <jeffhoglen@gmail.com>
2022-05-23 10:45:05 -0400

    code checked in

--- inky-match/src/app/app.component.spec.ts
+++ inky-match/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('inky-match');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('inky-match app is running!');
-  });
 });
--- inky-match/src/app/card/card.component.spec.ts
+++ inky-match/src/app/card/card.component.spec.ts
@@ -1,16 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
-
+  jasmine.clock().install();
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [CardComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +22,30 @@ describe('CardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('ngOninit calls to start game and modal', () => {
+    expect(component.ngOnInit).toBeTruthy();
+  });
+  it('start modal on click of button close modal',() => {
+    let modal = document.getElementById("startModal");
+    let start = document.getElementById('startButton');
+    let button = start;
+    button.click();
+    let noModal = modal.getAttribute("style");
+    
+    component.startGameModal();
+
+    fixture.whenStable().then(() => {
+      expect(noModal).toEqual("display: none;");
+    });
+    expect(component.startGameModal).toBeTruthy();
+  });
+
+  it('reStartGameModal shows the end modal then restarts', () => {     
+    jasmine.clock().tick(6500);
+
+    component.reStartGameModal();
+
+    expect(component.reStartGameModal).toBeTruthy();
+  });
 });
+
